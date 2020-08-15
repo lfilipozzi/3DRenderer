318380111a16c280df3172f4df42fc7d95472f4d
Louis Filipozzi <louis.filipozzi@gmail.com>
2020-08-15 14:58:49 -0700

    Modify CSM distance

--- src/scene.cpp
+++ src/scene.cpp
@@ -105,7 +105,7 @@ void Scene::update() {
     m_projection = m_camera.getProjectionMatrix();
     
     // Compute view and projection matrices of the light source
-    m_cascades = {-0.3f, -10.0f, -30.0f, -60.0f};
+    m_cascades = {-0.3f, -10.0f, -20.0f, -35.0f};
 //     m_view = m_light.getViewMatrix();
 //     m_projection = m_light.getProjectionMatrix(m_camera, m_cascades).at(2);
     m_lightSpace = m_light.getLightSpaceMatrix(m_camera, m_cascades);
